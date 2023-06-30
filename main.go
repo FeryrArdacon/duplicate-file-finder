276f55f06fb8a54f3ccb8a6bb64cf558635de428
Patrick Siegmund <patrick.siegmund@live.de>
2023-06-30 20:55:33 +0200

    little improvements

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 )
 
 type Params struct {
+	StartDir        string
 	IncludeAllFiles bool
 }
 
@@ -18,43 +19,47 @@ func main() {
 	fileHashes := make(map[string][]string)
 
 	if len(os.Args) < 2 {
-		log.Fatalln("Can you give me one directory path, please? With the option \"-a\" you can include hidden files to the duplicate check.")
-		return
+		log.Fatalln("can you give me one directory path, please? with the option \"-a\" you can include hidden files to the duplicate check.")
 	}
 
-	var params = Params{}
-	var startDir = ""
+	params, err := getParams()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	iterateFiles(params.StartDir, fileHashes, params)
+	output(fileHashes)
+}
+
+func getParams() (Params, error) {
+	params := Params{}
+
+	containsParam := func(arg string, param string) bool {
+		return strings.HasPrefix(arg, "-") &&
+			!strings.HasPrefix(arg, "--") &&
+			strings.Contains(arg, param)
+	}
 
 	for _, arg := range os.Args[1:] {
-		if !strings.HasPrefix(arg, "-") && startDir == "" {
-			startDir = arg
-		} else if !strings.HasPrefix(arg, "-") && startDir != "" {
-			log.Fatalln("Sorry, but I cannot search in multipe directory paths. Can you give me only one directory path, please?")
-			return
+		if !strings.HasPrefix(arg, "-") && params.StartDir == "" {
+			params.StartDir = arg
+		} else if !strings.HasPrefix(arg, "-") && params.StartDir != "" {
+			err := fmt.Errorf("sorry, but I cannot search in multipe directory paths. can you give me only one directory path, please?")
+			return Params{}, err
 		}
 
-		if strings.HasPrefix(arg, "-") && strings.Contains(arg, "a") {
+		if containsParam(arg, "a") {
 			params.IncludeAllFiles = true
 		}
 	}
 
-	iterateFiles(startDir, fileHashes, params)
-
-	for hash, filePaths := range fileHashes {
-		if len(filePaths) > 1 {
-			fmt.Printf("=== %s ===\n", hash)
-			for _, file := range filePaths {
-				fmt.Println(file)
-			}
-			fmt.Print("=========\n\n")
-		}
-	}
+	return params, nil
 }
 
 func iterateFiles(dirPath string, fileHashes map[string][]string, params Params) {
 	files, err := os.ReadDir(dirPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
 	for _, file := range files {
@@ -65,24 +70,39 @@ func iterateFiles(dirPath string, fileHashes map[string][]string, params Params)
 		filePath := filepath.Join(dirPath, file.Name())
 		if file.IsDir() {
 			iterateFiles(filepath.Join(filePath), fileHashes, params)
-		} else {
-			hash := fmt.Sprintf("%x", getHashForFile(filePath))
-			fileHashes[hash] = append(fileHashes[hash], filePath)
+			continue
 		}
+
+		hash := fmt.Sprintf("%x", getHashForFile(filePath))
+		fileHashes[hash] = append(fileHashes[hash], filePath)
 	}
 }
 
 func getHashForFile(filePath string) []byte {
 	fileForHashing, err := os.Open(filePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 	defer fileForHashing.Close()
 
 	hashSha256 := sha256.New()
 	if _, err := io.Copy(hashSha256, fileForHashing); err != nil {
-		log.Fatal(err)
+		log.Fatalln(err)
 	}
 
 	return hashSha256.Sum(nil)
+}
+
+func output(fileHashes map[string][]string) {
+	for hash, filePaths := range fileHashes {
+		if len(filePaths) < 2 {
+			continue
+		}
+
+		fmt.Printf("=== %s ===\n", hash)
+		for _, file := range filePaths {
+			fmt.Println(file)
+		}
+		fmt.Print("=========\n\n")
+	}
 }
